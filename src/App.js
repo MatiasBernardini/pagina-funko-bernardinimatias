aaef1316fb2262de9f735d716878cae29c641f91
matiasbernardini <bernardinimatias14@gmail.com>
2022-09-29 16:20:58 -0300

    navbar 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Header from './componentes/navbar/Navbar'
+import Header from './componentes/Navbar/Navbar'
 
 const App = () => {
   return (
