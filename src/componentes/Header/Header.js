b7a893ee1930b668047294b472d12b09a4bcbd26
matiasbernardini <bernardinimatias14@gmail.com>
2022-09-29 16:15:41 -0300

    navbar

---
+++ src/componentes/Header/Header.js
@@ -0,0 +1,23 @@
+import React from "react"
+import './Header.css'
+import logo from '../../assets/logo funko.png'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+const Header = () => {
+    return(
+       <header>
+        <img src={logo} alt="logoPagina" />
+        <h2>Funko Shop</h2>
+        <nav>
+            <a href="">Categoria 1</a>
+            <a href="">Categoria 2</a>
+            <a href="">Categoria 3</a>
+            <a href="">Categoria 4</a>
+        </nav>
+        <AddShoppingCartIcon sx={{ fontSize: 40 }}/>
+
+       </header>
+    )
+}
+
+export default Header 
