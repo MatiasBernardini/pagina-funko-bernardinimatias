b1f4545a713f66a5b20ac6f17e40c398729f367f
matiasbernardini <bernardinimatias14@gmail.com>
2022-10-09 16:40:50 -0300

    ItemCount

--- src/Containers/ItemListContainer.js
+++ src/Containers/ItemListContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import './ItemListContainer.css'
+import ItemCount from '../componentes/ItemCount/ItemCount'
 
 export const ItemListContainer = ({ greeting, mansaje }) => {
+  const onAdd = (count) => {
+    console.log(`El usuaro quiere agregar ${count} productos`);
+  };
+
   return (
     <>
       <p>{greeting}</p>
       <p>{mansaje}</p>
+      <ItemCount stock={10} initial={1} onAdd={onAdd}/>
     </>
   );
 };
---
+++ src/componentes/ItemCount/ItemCount.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+
+const ItemCount = ({stock, initial, onAdd}) => {
+    const [count, setCount] = useState (1);
+
+    const subtract = () => {
+        if (count > 1){
+            setCount (count - 1);
+        }
+    };
+
+    const add = () => {
+        if (count < stock){
+            setCount (count + 1);
+        }
+
+    };
+
+    return(
+        <div>
+            <button onClick={subtract}>-</button>
+            <h3>{count}</h3>
+            <button onClick={add}>+</button>
+            <button disabled={stock === 0 } onClick={() =>onAdd (count)}>
+                <h4> {stock === 0 ? "No hay Stock" : "Agregar al Carrito" } </h4>
+            </button>
+        </div>
+    );
+};
+
+export default ItemCount 
